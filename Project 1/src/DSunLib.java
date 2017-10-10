ac283c7dea95667fd100f2ec3de0411805fac760
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-10-10 09:10:48 -0400

    editz

--- Project 1/src/DSunLib.java
+++ Project 1/src/DSunLib.java
@@ -1,4 +1,13 @@
 
-public class DSunLib {
-
+public class DSunLib 
+{
+	//sumUpTo adds all numbers up to given value- done
+		public static void sumUpTo(int x) 
+		{int y=0;
+			for (int i=1; i<=x; i++) 
+			{
+				y=y+i;
+			}
+			System.out.print(y);
+		}
 }
